790cf34eff51d60066234aeb896df0e76eec305b
D4ryl3ne1 <Darylehen02@gmail.com>
2019-05-31 21:11:28 -0600

    initial project setup with webpack.config.file

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+const helperModule =  require('./my-helper-module');
+import * as _ from 'lodash';
+import './../styles/app.css';
+
+console.log("Welcome from app.js. Let's learn Webpack4!!!!!!");
+console.log(helperModule.greetings);
+
+var arr= [ 1, 2, 3];
+_.each(arr, function(val) {
+    console.log('Output from Lodash _.each for Element ' + val);
+});
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+var package = require('./package.json');
+//add a rule to use ‘css-loader’ and ‘style-loader’ for .css files.
+console.log(package.dependencies);
+
+module.exports = {
+    mode: 'production',
+    //changed the value for entry property from a string to an object with two keys -app and vendor
+    entry: {
+        app: "./src/scripts/index.js",
+       // vendor: Object.keys(package.dependencies),
+    }, 
+    output: { 
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js" 
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "../dist/"),
+        port: 9000
+    },
+
+    module:{
+        rules:[
+                {
+                    test:/\.(s*)css$/, //or  test: /\.scss/,
+                    use:[ 'style-loader','css-loader'
+                        /*
+                         {
+                         loader: 'style-loader',
+                         options: {
+                            sourceMap:true,
+                          },
+
+                         loader: 'css-loader', 
+                         options: {
+                            sourceMap: true,
+                          },
+
+                        loader: 'resolve-url-loader',
+                        options: {
+                            root: path.resolve(__dirname, 'src'),
+                            sourceMap: true,
+                        },
+                         
+
+                         loader: 'sass-loader',
+                         options: {
+                            context: path.resolve(__dirname, 'src'),
+                            root: path.resolve(__dirname, 'src'),
+                            sourceMap: true,
+                            includePaths: [path.resolve(__dirname, 'src/css'), path.resolve(__dirname, 'src')],
+                          },
+                        */
+                         
+                        ]
+                 }
+         ]
+      },
+    
+    //watch: true, //previously was watch
+    resolve: { 
+        extensions: [".js", ".ts", '.css'],
+        modules: [
+            'node_modules'
+          ]     
+    
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash:true,
+            title: 'Webpack-Demo-App',
+            myPageHeader: 'Hello World',
+            template: './src/index.html',
+            filename: 'index.html'
+        }) 
+    ]
+} 
+/*
+, we may want to create two bundles –app.bundle.js for all our application-specific code, 
+  and another bundle vendor.bundle.js for all our npm dependencies and include the two bundles 
+  in two separate <script> tags in your index.html. Lets how we can configure webpack to achieve this.
+*/
